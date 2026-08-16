761b36eb2ac45c24efe1fa07b320ee9cf3a02e12
JOSTGAPA <joffregar479@gmail.com>
2026-08-16 14:39:43 -0500

    Crear estructura base de carpetas y archivos para el semestre

--- script.js
+++
@@ -1,8 +0,0 @@
-const boton = document.getElementById('botonTema');
-const body = document.body;
-
-// Escucha el evento de clic en el botón
-boton.addEventListener('click', () => {
-    // Alterna la clase 'tema-oscuro' en el body
-    body.classList.toggle('tema-oscuro');
-});
